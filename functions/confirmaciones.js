cb337747b2a84999690980fb85a196420c1e8b20
Andres Mercado <andres23.t30@gmail.com>
2026-03-18 15:08:19 -0500

    pagos

--- functions/confirmaciones.js
+++ functions/confirmaciones.js
@@ -1,7 +1,7 @@
 import { delayEscritura } from "./typing.js";
 import { NUMERO_ADMIN } from "./config.js";
 
-const STICKER_PAGO_ID = "T9gVHBVeWDiTlhsDL0tr3Xjbu0bUzAlP1VjSHjf5rKg="; // TU STICKER REAL
+const STICKER_PAGO_ID = "T9gVHBVeWDiTlhsDL0tr3Xjbu0bUzAlP1VjSHjf5rKg=";
 
 export async function procesarConfirmacion(sock, msg, configGrupo) {
   const sticker = msg.message?.stickerMessage;
@@ -10,24 +10,33 @@ export async function procesarConfirmacion(sock, msg, configGrupo) {
   const stickerId = sticker.fileSha256?.toString("base64");
   if (stickerId !== STICKER_PAGO_ID) return;
 
+  // 🔥 USUARIO REAL (FIX)
+  const jidUsuario =
+    msg.key.participantPn ||
+    msg.key.participant ||
+    msg.key.remoteJid ||
+    "";
+
+  const numero = jidUsuario.split("@")[0];
+
   await delayEscritura(sock, msg.key.remoteJid);
 
   // 📩 MENSAJE AL GRUPO
   await sock.sendMessage(msg.key.remoteJid, {
     text: `✅ *Pago confirmado* 🧾🙌
 
-👤 Usuario: @${msg.key.participant.split("@")[0]}
+👤 Usuario: @${numero}
 📍 Grupo: *${configGrupo.nombre}*
 
 ⋆ ᴱᶠᵃᵃ𝐭 ⋆`,
-    mentions: [msg.key.participant]
+    mentions: [jidUsuario]
   });
 
   // 📲 MENSAJE AL ADMIN
   await sock.sendMessage(NUMERO_ADMIN, {
     text: `💰 *Pago confirmado*
 
-👤 Usuario: ${msg.key.participant}
+👤 Usuario: ${numero}
 📍 Grupo: ${configGrupo.nombre}`
   });
 
