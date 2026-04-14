dbf9c0613228856e1d12ab58367491013910022f
Fxandres30 <156185699+Fxandres30@users.noreply.github.com>
2026-04-14 09:56:27 -0500

    Actualizar grupos.js

--- functions/grupos.js
+++ functions/grupos.js
@@ -2,17 +2,17 @@ export const GRUPOS_PERMITIDOS = {
 
   "120363418497965618@g.us": {
     nombre: "#1 efaat Diarias",
-    tabla:  "reservas_dos_cifras" // 👈 NOMBRE REAL
+    tabla:  "5k_15k_reservas_2_cifras" // 👈 NOMBRE REAL
   },
   
   "120363422425898785@g.us": {
     nombre: "#2 efaat grupo",
-    tabla:  "5k_15k_reservas_2_cifras" // 👈 NOMBRE REAL
+    tabla:  "reservas_dos_cifras" // 👈 NOMBRE REAL
   },
 
   "120363420487085193@g.us": {
     nombre: "#3 Efaat Clup",
-    tabla:  "5k_15k_reservas_2_cifras" // 👈 NOMBRE REAL
+    tabla:  "reservas_dos_cifras" // 👈 NOMBRE REAL
   },
   
   "120363425078329823@g.us": {
